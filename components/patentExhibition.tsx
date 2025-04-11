89229de8951b47670f2d8beaf3ff718f87048bd4
Parth Kalani <parthkalani9282@gmail.com>
2025-04-11 23:21:24 +0530

    fixes

--- components/patentExhibition.tsx
+++ components/patentExhibition.tsx
@@ -5,39 +5,39 @@ import { BsAward } from "react-icons/bs";
 import { awards, patentExhibition } from "@/lib/data";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
-const parseDate = (dateString: string): [number, number, number] => {
-  const [monthName, dayRange, year] = dateString.split(/[\s,]+/);
-  const months: Record<string, number> = {
-    January: 1,
-    February: 2,
-    March: 3,
-    April: 4,
-    May: 5,
-    June: 6,
-    July: 7,
-    August: 8,
-    September: 9,
-    October: 10,
-    November: 11,
-    December: 12,
-  };
+// const parseDate = (dateString: string): [number, number, number] => {
+//   const [monthName, dayRange, year] = dateString.split(/[\s,]+/);
+//   const months: Record<string, number> = {
+//     January: 1,
+//     February: 2,
+//     March: 3,
+//     April: 4,
+//     May: 5,
+//     June: 6,
+//     July: 7,
+//     August: 8,
+//     September: 9,
+//     October: 10,
+//     November: 11,
+//     December: 12,
+//   };
 
-  const month = months[monthName] || 0;
-  const day = dayRange ? parseInt(dayRange.split("-")[0], 10) : 1;
-  return [month, day, parseInt(year, 10)];
-};
+//   const month = months[monthName] || 0;
+//   const day = dayRange ? parseInt(dayRange.split("-")[0], 10) : 1;
+//   return [month, day, parseInt(year, 10)];
+// };
 
 export default function PatentExhibition() {
   const { sectionRefs } = useActiveSectionContext();
 
-  const sortedAwards = [...patentExhibition].sort((a, b) => {
-    const [monthA, dayA, yearA] = parseDate(a.date);
-    const [monthB, dayB, yearB] = parseDate(b.date);
+  // const sortedAwards = [...patentExhibition].sort((a, b) => {
+  //   const [monthA, dayA, yearA] = parseDate(a.date);
+  //   const [monthB, dayB, yearB] = parseDate(b.date);
 
-    if (yearA !== yearB) return yearB - yearA;
-    if (monthA !== monthB) return monthB - monthA;
-    return dayB - dayA;
-  });
+  //   if (yearA !== yearB) return yearB - yearA;
+  //   if (monthA !== monthB) return monthB - monthA;
+  //   return dayB - dayA;
+  // });
 
   return (
     <section
@@ -48,7 +48,7 @@ export default function PatentExhibition() {
       <MainTitle
         heading={{ title: "Patent and Book exhibition", icon: BsAward }}
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-12 justify-center">
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-12 justify-center">
         {sortedAwards.map((award: any, index: number) => (
           <div
             key={index}
@@ -75,7 +75,7 @@ export default function PatentExhibition() {
             </h2>
           </div>
         ))}
-      </div>
+      </div> */}
       <hr className="mt-16 dark:bg-white bg-black dark:opacity-10" />
     </section>
   );
